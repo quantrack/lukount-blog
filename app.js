1edc47a20e917d2bef9440af525991e2517a7316
Hichem <hichem.barmou@gmail.com>
2020-10-05 12:08:09 +0200

    Added Compose page

--- app.js
+++ app.js
@@ -10,15 +10,34 @@ const postTitle = "Sed ut perspiciatis unde omnis iste natus error sit."
 
 const postContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus dolor eget nunc condimentum, ut tristique risus luctus. Praesent pulvinar vulputate tellus nec vulputate. In condimentum ex vitae tellus mollis faucibus. Nulla dictum scelerisque sapien quis vehicula. Ut est urna, venenatis at consectetur a, mattis quis sem. Maecenas feugiat, lacus quis condimentum porta, diam enim dapibus nulla, sit amet consectetur sapien nibh eget libero. Phasellus volutpat auctor diam sit amet dictum. Vivamus aliquam tortor quis augue ornare, eu eleifend nibh cursus. Curabitur mi arcu, molestie in diam non, sagittis congue enim. Quisque pharetra felis urna, at rhoncus diam aliquet non. Mauris euismod, odio non pulvinar euismod, lacus lorem lacinia elit, vitae auctor elit ante eu dui. Sed interdum gravida ipsum. Donec aliquet convallis lorem, et molestie dui pulvinar eget."
 
-
+let posts = []
 
 app.get("/", function(req,res){
-  res.render("bloghome", {startingContent:homestartingcontent})
+  res.render("bloghome", {
+    startingContent:homestartingcontent,
+    posts:posts
+})
 });
 
 app.get("/post", function(req,res){
-
   res.render("blogpost", {postTitle:postTitle, postContent:postContent})
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose")
+});
+
+app.post("/compose", function(req,res){
+
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postTitle
+  };
+
+posts.push(post);
+
+res.redirect("/")
+
 });
 
 
