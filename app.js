2a14b215b6b7ef705a1bd3eb44b410cbd3567554
Hichem <hichem.barmou@gmail.com>
2020-10-10 19:29:24 +0200

    Backend : added routes and models folders

--- app.js
+++ app.js
@@ -3,24 +3,28 @@ const app = express();
 const ejs = require ("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+
+
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/lukountblogDB", {useNewUrlParser: true,useUnifiedTopology:true});
 
 const loremTitle = "Sed ut perspiciatis unde omnis iste natus error sit."
 const postContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus dolor eget nunc condimentum, ut tristique risus luctus. Praesent pulvinar vulputate tellus nec vulputate. In condimentum ex vitae tellus mollis faucibus. Nulla dictum scelerisque sapien quis vehicula. Ut est urna, venenatis at consectetur a, mattis quis sem. Maecenas feugiat, lacus quis condimentum porta, diam enim dapibus nulla, sit amet consectetur sapien nibh eget libero. Phasellus volutpat auctor diam sit amet dictum. Vivamus aliquam tortor quis augue ornare, eu eleifend nibh cursus. Curabitur mi arcu, molestie in diam non, sagittis congue enim. Quisque pharetra felis urna, at rhoncus diam aliquet non. Mauris euismod, odio non pulvinar euismod, lacus lorem lacinia elit, vitae auctor elit ante eu dui. Sed interdum gravida ipsum. Donec aliquet convallis lorem, et molestie dui pulvinar eget."
 
+// Routing
+const router = express.Router();
+const compose = require ("./routes/compose");
+app.use("/compose", compose);
+
+
+//------------------
 // -------------Database----------
-const postSchema = new mongoose.Schema({
-  title: String,
-  content: String
-  })
+mongoose.connect("mongodb://localhost:27017/lukountblogDB", {useNewUrlParser: true,useUnifiedTopology:true});
+const Post = require("./models/post"); 
 
-const Post = mongoose.model("Post", postSchema);
 // ---------------------------------
-
-
 
 
 app.get("/", function(req,res){
@@ -36,34 +40,9 @@ app.get("/post", function(req,res){
   res.render("blogpost", {postTitle:postTitle, postContent:postContent})
 });
 
-app.get("/compose", function(req,res){
-  res.render("compose")
-});
 
-app.post("/compose", function(req,res){
-
-  const post = new Post({
-
-    title: req.body.postTitle,
-    content : req.body.postContent,
-  
-    });
-
-    post.save(function(err){
-      if (!err) {
-        res.redirect("/");
-      }
-    });
-
-});
-
-
-
-
-
-
-
-app.get("/post/:postId", function(req, res){
+app.route("/post/:postId")
+.get(function(req, res){
 
   const requestedPostId = req.params.postId;
   
---
+++ routes/compose.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post"); 
+
+
+
+
+
+router
+.route("/")
+.get(function(req,res){
+  res.render("compose")
+})
+.post(function(req,res){
+
+  const post = new Post({
+
+    title: req.body.postTitle,
+    content : req.body.postContent,
+
+    });
+
+    post.save(function(err){
+      if (!err) {
+        res.redirect("/");
+      }
+    });
+
+});
+
+module.exports = router;
